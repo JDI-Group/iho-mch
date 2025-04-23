cb09b4e38ebcd99d4a24c7d86b88c6f58c062d14
hairyf <wwu710632@gmail.com>
2025-04-23 17:17:30 +0800

    style: remove unnecessary padding in product page layout

--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -19,7 +19,7 @@ function Page() {
 
   return (
     <layouts.default>
-      <If cond={!loading} tag="section" className="flex flex-col gap-4 px-0 md:px-8 py-8 md:py-10">
+      <If cond={!loading} tag="section" className="flex flex-col gap-4 px-0 py-8 md:py-10">
         <Then cond={detail} tag="div">
           <div className="flex flex-col md:flex-row gap-2 md:gap-14">
             <div className="flex-1 flex flex-col gap-2">
