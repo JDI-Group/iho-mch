6b924edbaeacbee4cb1d7b9dc74e524b8d9eba47
hairyf <wwu710632@gmail.com>
2025-05-13 14:13:53 +0800

    chore: add pnpm workspace configuration and update TypeScript settings
    - Created a new pnpm-workspace.yaml file to manage packages.
    - Updated TypeScript configuration:
      - Changed target from ES2017 to ESNext.
      - Updated module resolution from bundler to node.
      - Set noImplicitAny to false for better compatibility.
      - Removed unnecessary paths configuration and types for vitest/globals.

---
+++ packages/solidity/contracts/IHOMining.sol
@@ -0,0 +1,187 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./utils/VerifiableUpgradeable.sol";
+import "./utils/BidirectionalTransfer.sol";
+
+contract IHOMining is 
+  VerifiableUpgradeable,
+  BidirectionalTransfer,
+  UUPSUpgradeable,
+  OwnableUpgradeable,
+  ERC721Upgradeable {
+  
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() { _disableInitializers(); }
+  function _authorizeUpgrade(address) internal override onlyOwner {}
+
+  struct DeviceMapping {
+    uint256 timestamp;
+    int256 height;
+    string device;
+  }
+
+  struct TokenMapping {
+    address token;
+    uint256 tokenId;
+  }
+
+  struct Reward {
+    address token;
+    uint256 amount;
+  }
+
+  uint256 private _tokenId;
+  uint128 public totalSupply;
+  mapping(string => TokenMapping) private DeviceMapToken;
+  mapping(address => mapping(uint256 => DeviceMapping)) private TokenMapDevice;
+  mapping(address => mapping(uint256 => uint256)) private TokenMapPool;
+  mapping(string => bool) private ReceivedIDs;
+
+  event Registered(
+    address indexed token,
+    uint256 indexed tokenId,
+    address indexed sender,
+    string device,
+    int256 blockHeight,
+    uint256 timestamp
+  );
+  event Received(
+    address indexed token,
+    uint256 indexed tokenId,
+    string uid,
+    string device,
+    address to,
+    uint256 amount,
+    string memo,
+    int256 blockHeight,
+    uint256 timestamp
+  );
+  event Claimed(
+    address indexed token,
+    uint256 indexed tokenId,
+    string device,
+    address to,
+    uint256 amount,
+    int256 blockHeight,
+    uint256 timestamp
+  );
+
+  error DeviceEmpty();
+  error DeviceRegistered();
+  error DeviceUnregistered();
+  error ReceiveInvalid();
+
+  function initialize(address _owner, address _verifier) public initializer {
+    __ERC721_init("IHOMining", "IHOM");
+    __Verifie_init(_verifier);
+    __Ownable_init(_owner);
+    __UUPSUpgradeable_init();
+  }
+
+  function mint(address signer) private returns (uint256) {
+    uint256 tokenId = _tokenId;
+    _safeMint(signer, tokenId);
+    totalSupply += 1;
+    _tokenId += 1;
+    return tokenId;
+  }
+
+  function register(string memory device) public {
+    if (bytes(device).length == 0)
+      revert DeviceEmpty();
+    if (DeviceMapToken[device].token != address(0))
+      revert DeviceRegistered();
+
+    uint256 tokenId = mint(msg.sender);
+
+    TokenMapDevice[address(this)][tokenId] = DeviceMapping(block.timestamp, int(block.number), device);
+    DeviceMapToken[device] = TokenMapping(address(this), tokenId);
+
+    emit Registered(
+      address(this),
+      tokenId,
+      msg.sender,
+      device,
+      int(block.number),
+      block.timestamp
+    );
+  }
+
+  function rewardReceive(
+    string memory id,
+    address token,
+    uint256 tokenId,
+    string memory device,
+    uint256 amount,
+    bytes memory signature,
+    string memory memo
+  ) public {
+    TokenMapping memory tm = DeviceMapToken[device];
+    address owner = IERC721(tm.token).ownerOf(tm.tokenId);
+
+    if (token == address(0))
+      revert DeviceUnregistered();
+    if (owner != msg.sender)
+      revert ERC721IncorrectOwner(msg.sender, tm.tokenId, owner);
+    if (bytes(device).length == 0)
+      revert DeviceEmpty();
+    if (ReceivedIDs[id])
+      revert ReceiveInvalid();
+
+    verify(abi.encodePacked(id, msg.sender, device, amount), signature);
+    
+    ReceivedIDs[id] = true;
+
+    TokenMapPool[token][tokenId] += amount;
+
+    emit Received(
+      tm.token,
+      tm.tokenId, 
+      id,
+      device,
+      msg.sender, 
+      amount,
+      memo,
+      int(block.number),
+      block.timestamp
+    );
+  }
+
+  function rewardClaim(
+    address token,
+    uint256 tokenId,
+    string memory device,
+    uint256 amount,
+    bytes memory signature
+  ) public {
+    TokenMapping memory tm = DeviceMapToken[device];
+    address owner = IERC721(tm.token).ownerOf(tm.tokenId);
+
+    if (token == address(0))
+      revert DeviceUnregistered();
+    if (owner != msg.sender)
+      revert ERC721IncorrectOwner(msg.sender, tm.tokenId, owner);
+    if (bytes(device).length == 0)
+      revert DeviceEmpty();
+
+    verify(abi.encodePacked(msg.sender, device, amount), signature);
+    transfer(address(this), msg.sender, address(0), amount);
+
+    TokenMapPool[token][tokenId] -= amount;
+
+    emit Claimed(
+      tm.token,
+      tm.tokenId, 
+      device,
+      msg.sender, 
+      amount,
+      int(block.number),
+      block.timestamp
+    );
+  }
+}
---
+++ packages/solidity/harsta.config.ts
@@ -0,0 +1,81 @@
+import type { Deploy } from 'harsta'
+import process from 'node:process'
+import { Wallet } from 'ethers'
+import { defineConfig } from 'harsta'
+
+const deploy: Deploy = {
+  accounts: [
+    process.env.DEPLOYER_PRIVATE_KEY || Wallet.createRandom().privateKey,
+    process.env.VERIFIER_PRIVATE_KEY || Wallet.createRandom().privateKey,
+  ],
+  saveDeployments: true,
+  allowUnlimitedContractSize: true,
+  gas: 'auto',
+  gasPrice: 'auto',
+}
+
+const config = defineConfig({
+  solidity: {
+    settings: { evmVersion: 'shanghai' },
+    version: '0.8.24',
+  },
+  namedAccounts: {
+    deployer: { default: 0 },
+    verifier: { default: 1 },
+  },
+  networks: {
+    moonchain_geneva: {
+      name: 'Moonchain Testnet',
+      rpc: 'https://geneva-rpc.moonchain.com',
+      testnet: true,
+      id: 5167004,
+      icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
+      currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
+      explorer: {
+        name: 'etherscan',
+        url: 'https://geneva-explorer.moonchain.com',
+      },
+      deploy,
+    },
+    moonchain: {
+      name: 'Moonchain',
+      rpc: 'https://rpc.mxc.com',
+      id: 18686,
+      icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
+      currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
+      explorer: {
+        name: 'etherscan',
+        url: 'https://explorer.moonchain.com',
+      },
+      deploy,
+    },
+  },
+  deployments: {
+    IHOMarket: {
+      kind: 'uups',
+      args: async ({ getNamedAccount }) => {
+        return [
+          await getNamedAccount('deployer'),
+          await getNamedAccount('verifier'),
+        ]
+      },
+    },
+    IHOMining: {
+      kind: 'uups',
+      args: async ({ getNamedAccount }) => {
+        return [
+          await getNamedAccount('deployer'),
+          await getNamedAccount('verifier'),
+        ]
+      },
+    },
+    IHOFuel: {
+      kind: 'uups',
+      args: async ({ getNamedAccount }) => {
+        return [await getNamedAccount('deployer')]
+      },
+    },
+  },
+})
+
+export default config
