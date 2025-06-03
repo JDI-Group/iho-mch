b861e1cc83c3fcd15882ed8aaf09078d4ea0375f
hairyf <wwu710632@gmail.com>
2025-06-03 14:34:49 +0800

    fix: update signature verification parameter to use hashed message

--- packages/solidity/contracts/utils/VerifiableUpgradeable.sol
+++ packages/solidity/contracts/utils/VerifiableUpgradeable.sol
@@ -22,14 +22,14 @@ abstract contract VerifiableUpgradeable is Initializable {
 
   /**
    * @dev Verifies a message signature
-   * @param message The original message (not hashed)
+   * @param hash The hash of the message to verify
    * @param signature The signature data
    *
    * Note: This function hashes the message with keccak256 before verifying the signature.
    * When signing on the client side, the same message should be hashed before signing.
    */
-  function verify(bytes memory message, bytes memory signature) internal view virtual {
-    bytes32 keccak256SignedMessage = MessageHashUtils.toEthSignedMessageHash(keccak256(message));
+  function verify(bytes memory hash, bytes memory signature) internal view virtual {
+    bytes32 keccak256SignedMessage = MessageHashUtils.toEthSignedMessageHash(keccak256(hash));
     address recoveredAddress = ECDSA.recover(keccak256SignedMessage, signature);
     if (verifier != recoveredAddress) {
       revert InvalidSignature(recoveredAddress, verifier);
