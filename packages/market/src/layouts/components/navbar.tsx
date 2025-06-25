4d4defe4a14b9a0bd9307c34f03bd134e5c7c962
hairyf <wwu710632@gmail.com>
2025-06-25 15:55:43 +0800

    chore: update view

--- packages/market/src/layouts/components/navbar.tsx
+++ packages/market/src/layouts/components/navbar.tsx
@@ -35,7 +35,7 @@ export function Navbar(props: NavbarProps) {
         <NavbarBrand className="mr-4 gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             {/* <Logo /> */}
-            <img className="w-10" src="https://raw.githubusercontent.com/MXCzkEVM/metadata/refs/heads/main/logo-circle.svg" />
+            <img className="w-8" src="https://raw.githubusercontent.com/MXCzkEVM/metadata/refs/heads/main/logo-circle.svg" />
             {/* <span className="font-spacex mt-1 text-xs sm:text-base">
               MOONCHAIN
             </span> */}
