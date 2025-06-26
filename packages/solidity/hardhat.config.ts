f4add172a6f8f5e6c086d23cc3c70231b9309878
hairyf <wwu710632@gmail.com>
2025-06-26 22:44:01 +0800

    fix(moonchain): update RPC URL for Moonchain network

--- packages/solidity/hardhat.config.ts
+++ packages/solidity/hardhat.config.ts
@@ -47,7 +47,7 @@ const config = {
       currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
       explorer: { name: 'etherscan', url: 'https://explorer.moonchain.com' },
       icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
-      url: 'http://207.246.101.30:8545',
+      url: 'https://rpc.mxc.com',
       type: 'http',
       chainId: 18686,
       chainType: 'l1',
