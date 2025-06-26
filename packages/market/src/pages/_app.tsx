11dbd8a853ac17572c65de5f3a579351099f8132
hairyf <wwu710632@gmail.com>
2025-06-26 18:33:27 +0800

    chore: update dependencies to latest versions

--- packages/market/src/pages/_app.tsx
+++ packages/market/src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from 'next/app'
 import { BootstrapProvider } from '@/components/bootstrap-provider'
 import { InjectsProvider } from '@/components/injects-provider'
-import { Injector } from '@hairy/react-lib'
+import { Injector, useWatch } from '@hairy/react-lib'
+import { pageviewCount } from '@waline/client'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import '@/styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
+  useWatch(
+    router.asPath,
+    () => pageviewCount({ serverURL: 'waline-swart-eta.vercel.app', path: window.location.pathname }),
+    { immediate: true },
+  )
   return (
     <NextThemesProvider defaultTheme="dark">
       <Injector install={[InjectsProvider, BootstrapProvider]}>
