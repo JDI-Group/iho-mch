03dbd078db1bb858f1dc2f585b407359729dc321
hairyf <wwu710632@gmail.com>
2025-06-27 17:16:53 +0800

    chore: update configuration files for consistency

--- packages/solidity/hardhat.config.ts
+++ packages/solidity/hardhat.config.ts
@@ -66,7 +66,7 @@ const config = {
       currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
       explorer: { name: 'etherscan', url: 'https://explorer.moonchain.com' },
       icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
-      url: 'http://207.246.101.30:8545',
+      url: 'https://rpc.moonchain.com',
       type: 'http',
       chainId: 18686,
       chainType: 'l1',
