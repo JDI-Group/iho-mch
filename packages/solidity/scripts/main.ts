6bb7cbdad6dc67bf4629aff5e0cbef7c75241ba6
hairyf <wwu710632@gmail.com>
2025-07-01 19:39:23 +0800

    refactor: main script to connect to 'moonchain' and confirm product 22714; removed unused log retrieval and commented-out code.

--- packages/solidity/scripts/main.ts
+++ packages/solidity/scripts/main.ts
@@ -1,29 +1,15 @@
 import { network } from 'hardhat'
-import { getAbiItem } from 'viem'
 import IHOLockVaultV1Module from '../ignition/modules/IHOLockVaultV1'
 
 async function main() {
-  const { ignition, viem } = await network.connect('moonchainGeneva')
-  const client = await viem.getPublicClient()
+  const { ignition, viem } = await network.connect('moonchain')
   const { market } = await ignition.deploy(IHOLockVaultV1Module)
 
-  const logs = await client.getLogs({
-    fromBlock: 0n,
-    address: market.address,
-    event: getAbiItem({ abi: market.abi, name: 'ProjectConfirm' }),
-  })
-
-  // await market.write.confirm([22643n])
-  // console.log('Confirmed product 22643')
-  // await market.write.confirm([22649n])
-  // console.log('Confirmed product 22649')
-  // await market.write.confirm([22668n])
-  // console.log('Confirmed product 22668')
+  await market.write.confirm([22714n])
+  console.log('Confirmed product 22714')
 
   // await market.write.finish()
   // console.log('Finished confirming products')
-
-  console.log(logs)
 }
 
 main()
