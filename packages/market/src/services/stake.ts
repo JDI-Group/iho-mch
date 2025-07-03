b61d13204561a7cee069f71c03a29ee057dea4ac
hairyf <wwu710632@gmail.com>
2025-07-03 20:40:33 +0800

    fix(stake): correct getter method for checking last state

--- packages/market/src/services/stake.ts
+++ packages/market/src/services/stake.ts
@@ -8,7 +8,7 @@ export async function helperStake(params: { product: number, variation?: number 
 export async function helperStake(params: { order: number } | { product: number, variation?: number }) {
   const { order, product, variation } = params as { order?: number, product?: number, variation?: number }
 
-  if (!store.config.$state.isLasted) {
+  if (!store.config.$getters.isLasted) {
     addToast({ description: 'Coming soon', color: 'danger' })
     return
   }
