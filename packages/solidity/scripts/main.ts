575fac223a56b43b7b77d6cc4965625f4a56614b
hairyf <wwu710632@gmail.com>
2025-07-22 20:03:27 +0800

    chore: update change

--- packages/solidity/scripts/main.ts
+++ packages/solidity/scripts/main.ts
@@ -1,14 +1,15 @@
 import { network } from 'hardhat'
-import IHOLockVaultV4Module from '../ignition/modules/IHOLockVaultV4'
+import IHOLockVaultV5Module from '../ignition/modules/IHOLockVaultV5'
 
 async function main() {
   const { ignition } = await network.connect('moonchain')
-  const { market } = await ignition.deploy(IHOLockVaultV4Module)
+  const { market } = await ignition.deploy(IHOLockVaultV5Module)
 
   const products = [
-    22714n,
+    23033n,
     22701n,
   ]
+
   for (const product of products) {
     await market.write.confirm([product])
     console.log(`Confirmed product $${product}`)
