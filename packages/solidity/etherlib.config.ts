c456990949bdc2fc4ea49e447a7eb711973c7150
hairyf <wwu710632@gmail.com>
2025-08-01 18:55:19 +0800

    chore: remove useless address configurations and simplify the configuration files for Ethereum library generators

--- packages/solidity/etherlib.config.ts
+++ packages/solidity/etherlib.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'etherlib-generator'
 import { ethers, hardhat, wagmi } from 'etherlib-generator/plugins'
-import { erc20Abi, zeroAddress } from 'viem'
+import { erc20Abi } from 'viem'
 
 // This is a configuration file for the Ethereum Library Generator.
 const config = defineConfig([
@@ -8,10 +8,6 @@ const config = defineConfig([
     output: '../mining/src/generated',
     fragments: { ERC20: erc20Abi },
 
-    addresses: {
-      IHOFueltank: { 18686: zeroAddress },
-      IHOMining: { 18686: zeroAddress },
-    },
     plugins: [
       hardhat(),
       wagmi(),
